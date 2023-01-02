169b8c116b7daafe715430203952fd1ce46021de
aminemode <dahmar529@gmail.com>
2023-01-02 12:02:20 +0100

    route

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+const bodyParser = require('body-parser');
 const express = require('express')
+const mongoose = require("mongoose");
 const app = express()
 const port = 3000
 require('./models/dbConfig');
 const postsRouter = require('./routes/postsController');
 
-app.use('/', postsRouter);
+// mongoose.set('strictQuery', true);
+
+app.use(bodyParser.json())
+app.use('/posts', postsRouter)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- routes/postsController.js
+++ routes/postsController.js
@@ -1,13 +1,59 @@
 const express = require('express');
 const router = express.Router();
+const ObjectId = require('mongoose').Types.ObjectId
 
 const { PostsModel } = require('../models/postsModel');
 
 router.get('/', (req, res) => {
     PostsModel.find((err, docs) => {
         if(!err) res.send(docs);
-        else console.log(err);
+        else console.log('get error ' + err);
     })
+})
+
+router.post('/', (req, res) => {
+    const newRecord = new PostsModel({
+        author: req.body.author,
+        message: req.body.message
+    })
+
+    newRecord.save((err, docs) => {
+        if (!err) res.send(docs)
+        else console.log('post error' + err)
+    })
+})
+
+router.put('/:id', (req, res) => {
+    if(!ObjectId.isValid(req.params.id))
+        return res.status(400).send('id inconue : ' + req.params.id)
+    
+    const updateRecord = {
+        author: req.body.author,
+        message: req.body.message
+    }
+
+    PostsModel.findByIdAndUpdate(
+        req.params.id,
+        { $set: updateRecord },
+        { new: true },
+        (err, docs) => {
+            if(!err) res.send(docs)
+            else console.log('update error ' + err)
+        }
+    )
+})
+
+router.delete('/:id', (req, res) => {
+    if(!ObjectId.isValid(req.params.id))
+        return res.status(400).send('id inconue : ' + req.params.id)
+
+    PostsModel.findByIdAndDelete(
+        req.params.id,
+        (err, docs) => {
+            if (!err) res.send(docs)
+            else console.log('delete error : ' + err)
+        }
+    )
 })
 
 module.exports = router
